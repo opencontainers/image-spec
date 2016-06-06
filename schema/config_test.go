c04fca2d7183cf6bbf581c5b3b56bda53c881c02
Sergiusz Urbaniak <sur@coreos.com>
2016-06-06 12:19:23 +0200

    schema: add more tests
    Currently samples in the specification are being tested. This adds
    additional tests.
    
    Fixes #83
    
    Signed-off-by: Sergiusz Urbaniak <sur@coreos.com>

---
+++ schema/config_test.go
@@ -0,0 +1,165 @@
+// Copyright 2016 The Linux Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schema_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/opencontainers/image-spec/schema"
+)
+
+func TestConfig(t *testing.T) {
+	for i, tt := range []struct {
+		config string
+		fail   bool
+	}{
+		// expected failure: field "os" has numeric value, must be string
+		{
+			config: `
+{
+    "architecture": "amd64",
+    "os": 123
+}
+`,
+			fail: true,
+		},
+
+		// expected failure: field "config.User" has numeric value, must be string
+		{
+			config: `
+{
+    "created": "2015-10-31T22:22:56.015925234Z",
+    "author": "Alyssa P. Hacker &ltalyspdev@example.com&gt",
+    "architecture": "amd64",
+    "os": "linux",
+    "config": {
+        "User": 1234
+    }
+}
+`,
+			fail: true,
+		},
+
+		// expected failue: history has string value, must be an array
+		{
+			config: `
+{
+    "history": "should be an array"
+}
+`,
+			fail: true,
+		},
+
+		// expected failure: Env has numeric value, must be a string
+		{
+			config: `
+{
+    "config": {
+        "Env": [
+            7353
+        ]
+    }
+}
+`,
+			fail: true,
+		},
+
+		// expected failure: config.Volumes has string array, must be an object (string set)
+		{
+			config: `
+{
+    "config": {
+        "Volumes": [
+            "/var/job-result-data",
+            "/var/log/my-app-logs"
+        ]
+    }
+}
+`,
+			fail: true,
+		},
+
+		// expected failue: invalid JSON
+		{
+			config: `invalid JSON`,
+			fail:   true,
+		},
+
+		{
+			config: `
+{
+    "created": "2015-10-31T22:22:56.015925234Z",
+    "author": "Alyssa P. Hacker &ltalyspdev@example.com&gt",
+    "architecture": "amd64",
+    "os": "linux",
+    "config": {
+        "User": "1:1",
+        "Memory": 2048,
+        "MemorySwap": 4096,
+        "CpuShares": 8,
+        "ExposedPorts": {
+            "8080/tcp": {}
+        },
+        "Env": [
+            "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+            "FOO=docker_is_a_really",
+            "BAR=great_tool_you_know"
+        ],
+        "Entrypoint": [
+            "/bin/sh"
+        ],
+        "Cmd": [
+            "--foreground",
+            "--config",
+            "/etc/my-app.d/default.cfg"
+        ],
+        "Volumes": {
+            "/var/job-result-data": {},
+            "/var/log/my-app-logs": {}
+        },
+        "WorkingDir": "/home/alice"
+    },
+    "rootfs": {
+      "diff_ids": [
+        "sha256:9d3dd9504c685a304985025df4ed0283e47ac9ffa9bd0326fddf4d59513f0827",
+        "sha256:2b689805fbd00b2db1df73fae47562faac1a626d5f61744bfe29946ecff5d73d"
+      ],
+      "type": "layers"
+    },
+    "history": [
+      {
+        "created": "2015-10-31T22:22:54.690851953Z",
+        "created_by": "/bin/sh -c #(nop) ADD file:a3bc1e842b69636f9df5256c49c5374fb4eef1e281fe3f282c65fb853ee171c5 in /"
+      },
+      {
+        "created": "2015-10-31T22:22:55.613815829Z",
+        "created_by": "/bin/sh -c #(nop) CMD [\"sh\"]",
+        "empty_layer": true
+      }
+    ]
+}
+`,
+			fail: false,
+		},
+	} {
+		r := strings.NewReader(tt.config)
+		err := schema.MediaTypeImageSerializationConfig.Validate(r)
+
+		if got := err != nil; tt.fail != got {
+			t.Errorf("test %d: expected validation failure %t but got %t, err %v", i, tt.fail, got, err)
+		}
+	}
+}
