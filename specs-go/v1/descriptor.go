3e67fa30043d480ea32404531ce5402e492045d5
Kirtana Ashok <kiashok@microsoft.com>
2023-06-06 16:43:38 -0700

    Add toml markup for Platform struct
    Signed-off-by: Kirtana Ashok <kiashok@microsoft.com>

--- specs-go/v1/descriptor.go
+++ specs-go/v1/descriptor.go
@@ -53,20 +53,20 @@ type Descriptor struct {
 type Platform struct {
 	// Architecture field specifies the CPU architecture, for example
 	// `amd64` or `ppc64le`.
-	Architecture string `json:"architecture"`
+	Architecture string `toml:"architecture" json:"architecture"`
 
 	// OS specifies the operating system, for example `linux` or `windows`.
-	OS string `json:"os"`
+	OS string `toml:"os" json:"os"`
 
 	// OSVersion is an optional field specifying the operating system
 	// version, for example on Windows `10.0.14393.1066`.
-	OSVersion string `json:"os.version,omitempty"`
+	OSVersion string `toml:"os_version,omitempty" json:"os.version,omitempty"`
 
 	// OSFeatures is an optional field specifying an array of strings,
 	// each listing a required OS feature (for example on Windows `win32k`).
-	OSFeatures []string `json:"os.features,omitempty"`
+	OSFeatures []string `toml:"os_features,omitempty" json:"os.features,omitempty"`
 
 	// Variant is an optional field specifying a variant of the CPU, for
 	// example `v7` to specify ARMv7 when architecture is `arm`.
-	Variant string `json:"variant,omitempty"`
+	Variant string `toml:"variant,omitempty" json:"variant,omitempty"`
 }
