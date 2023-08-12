0c1622e563500acefc9c7e272c499946cd91ebba
Michal Biesek <michalbiesek@gmail.com>
2023-08-12 11:52:20 +0200

    Add `riscv64` arch to `check{Architecture|Platform}`
    Signed-off-by: Michal Biesek <michalbiesek@gmail.com>

--- schema/validator.go
+++ schema/validator.go
@@ -212,6 +212,7 @@ func checkArchitecture(Architecture string, Variant string) {
 		"mips64":   {""},
 		"mips64le": {""},
 		"s390x":    {""},
+		"riscv64":  {""},
 	}
 	for arch, variants := range validCombins {
 		if arch == Architecture {
@@ -232,7 +233,7 @@ func checkPlatform(OS string, Architecture string) {
 		"darwin":    {"386", "amd64", "arm", "arm64"},
 		"dragonfly": {"amd64"},
 		"freebsd":   {"386", "amd64", "arm"},
-		"linux":     {"386", "amd64", "arm", "arm64", "ppc64", "ppc64le", "mips64", "mips64le", "s390x"},
+		"linux":     {"386", "amd64", "arm", "arm64", "ppc64", "ppc64le", "mips64", "mips64le", "s390x", "riscv64"},
 		"netbsd":    {"386", "amd64", "arm"},
 		"openbsd":   {"386", "amd64", "arm"},
 		"plan9":     {"386", "amd64"},
